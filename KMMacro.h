93a656e2e95f5188f885b11d6eda848b436c10f9
Kimi (Kimiko on MacBook Pro) <kimizqh@foxmail.com>
2017-02-08 16:25:49 +0800

    add: KMMacro.h

---
+++ KMMacro.h
@@ -0,0 +1,19 @@
+//
+//  KMMacro.h
+//  ssss
+//
+//  Created by 周祺华 on 2017/2/8.
+//  Copyright © 2017年 周祺华. All rights reserved.
+//
+
+#ifndef KMMacro_h
+#define KMMacro_h
+
+#define swapValue(type, x, y)   {   type _tmp_tmp_tmp_; \
+                                    _tmp_tmp_tmp_ = y;  \
+                                    y = x;              \
+                                    x = _tmp_tmp_tmp_;  \
+                                }
+
+
+#endif /* KMMacro_h */
